5eaa9ca07cf217752711aaf2fb7dfd59a4b67bc6
Ramaadit18 <ramaawp10@gmail.com>
2024-05-24 01:05:00 +0700

    Upload Files

---
+++ src/services/history.js
@@ -0,0 +1,22 @@
+const { Firestore } = require('@google-cloud/firestore');
+
+async function getHistories() {
+    const db = new Firestore();
+
+    return new Promise((resolve, reject) => {
+        db.collection('predictions').get()
+            .then(snapshot => {
+                const data = [];
+                snapshot.forEach(document => {
+                    data.push({ id: document.id, history: { ...document.data() } });
+                });
+                resolve(data);
+            })
+            .catch(error => {
+                console.error('Gagal menampilkan histories: ', error);
+                reject(error);
+            });
+    });
+}
+
+module.exports = getHistories;
